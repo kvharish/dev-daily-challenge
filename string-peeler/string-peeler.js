b7bc61720ae8aa0cf3a9b6f70608ee2d28e0eacf
K.V.Harish <coolharish27@gmail.com>
2019-10-02 21:13:33 +0530

    Update string-peeler.js
    Added challenge description as a comment to the string-peeler.js file

--- string-peeler/string-peeler.js
+++ string-peeler/string-peeler.js
@@ -1,1 +1,5 @@
+/* Your goal is to create a function that removes the first and last letters of a string.
+Strings with two characters or less are considered invalid.
+You can choose to have your function return null or simply ignore. */
+
 const stringPeeler = (str) => str.length > 2 ? str.substr(1, str.length - 2) : `Invalid input to stringPeeler function ${str}`;
